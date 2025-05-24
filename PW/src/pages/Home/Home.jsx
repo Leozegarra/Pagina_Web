ab9be646a90572e1318c2de70fc5cc09a13b64f6
Leozegarra <leandrozegarra570@gmail.com>
2025-05-23 23:21:05 -0500

    Mis cambios en la rama Leandro

--- PW/src/pages/Home/Home.jsx
+++ PW/src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import ProductCard from './components/ProductCard'
 
 const Home = () => {
   const products = [
-    { id: 1, name: 'Producto 1', price: 100, image: 'https://via.placeholder.com/150' },
+    { id: 1, name: 'Producto 1', price: 100, image: 'https://cdn.dribbble.com/userupload/3875672/file/original-42f52449520e8e7940c668566888d84f.png?resize=752x&vertical=center' },
     { id: 2, name: 'Producto 2', price: 200, image: 'https://via.placeholder.com/150' },
     { id: 3, name: 'Producto 3', price: 300, image: 'https://via.placeholder.com/150' }
   ]
